65458f5d763116ccc515bb1b7b4a7d56629412a2
KyleLee22 <46007982+KyleLee22@users.noreply.github.com>
2022-04-07 08:06:56 -0400

    Revert "added selector to graph, changed 2 crypto names"
    This reverts commit 971b363b366f21da2cbde20c1aee92142b3d14e5.

--- query_data.py
+++ query_data.py
@@ -43,8 +43,8 @@ def get_data(date1, date2, coin_string):
         "Bitcoin": "BTC-USD",
         "Ethereum": "ETH-USD",
         "Litecoin": "LTC-USD",
-        "Ripple": "XRP-USD",
-        "Bitcoin Cash": "BCH-USD"
+        "XRP": "XRP-USD",
+        "BitCash": "BCH-USD"
     }
     coin = string_to_coin(coin_string)
     check_dates(coin_symbols[coin_string])
@@ -99,8 +99,8 @@ def string_to_coin(coin_string):
         "Bitcoin": btc,
         "Ethereum": eth,
         "Litecoin": ltc,
-        "Bitcoin Cash": bch,
-        "Ripple": xrp
+        "BitCash": bch,
+        "XRP": xrp
     }[coin_string]
     return coin
 

db80a7adaa174a0667338f27f33c0c7a3cf622b6
erikim8 <erikim22@bergen.org>
2022-04-28 20:28:52 -0400

    Attempt at Uploading Files
    various changes to implent uploading of csv files

--- importcoin.py
+++ importcoin.py
@@ -1,25 +1,30 @@
 import pandas as pd
+import sys
 from pymongo import MongoClient
 from upload_data import *
 
-client = MongoClient("mongodb+srv://yalfan22:yale2004@cluster0.qszrw.mongodb.net/test", connect=False)
+client = MongoClient("mongodb+srv://erikim22:KIm3kEf1JKMOsbXx@cluster0.qszrw.mongodb.net/test", connect=False)
 db = client.pairs_trading
 
-btc = db.btc
-eth = db.eth
-ltc = db.ltc
-bch = db.bch
-xrp = db.xrp
-
-def importCoin(csv_path, db_name, coll_name, db_url='localhost', db_port=27000):
+def importCoin(csv_file, collection_name):
     """ Imports a csv file at path csv_name to a mongo colection
     returns: count of the documants in the new collection
     """
-    client = MongoClient(db_url, db_port)
-    db = client[db_name]
-    coll = db[coll_name]
-    data = pd.read_csv(csv_path)
-    payload = json.loads(data.to_json(orient='records'))
-    coll.remove()
-    coll.insert(payload)
-    return coll.count()
+    collection = db[collection_name]
+    sheet = pd.read_csv(csv_file)
+    data = []
+    rows = len(sheet.index)
+    for i in range(rows):
+        date = sheet.at[i, 'date']
+        d1 = datetime.datetime.strptime(date, "%Y-%m-%d")
+        high = sheet.at[i, 'high']
+        low = sheet.at[i, 'low']
+        open = sheet.at[i, 'open']
+        close = sheet.at[i, 'close']
+        volume = float(sheet.at[i, 'volume'])
+        avg = (high + low) / 2
+        element = {
+            "Date": d1, "Open": open, "High": high, "Low": low, "Close": close, "Average": avg, "Volume": volume
+        }
+        data.append(element)
+    collection.insert_many(data)

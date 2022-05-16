9d8dea5cd7fda347c173f0962b658a79aba9cb56
yale <yalfan22@bergen.org>
2022-05-16 09:47:27 -0400

    test

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import datetime
 from upload_data import *
+import tempfile
 
-client = MongoClient("mongodb+srv://yalfan22:yale2004@cluster0.qszrw.mongodb.net/test", connect=False)
-db = client.pairs_trading
-print(type(db.bch))
-coin = db.list_collection_names()[0]
-print(type(db.coin))
-print(coin)
+temp_dir = tempfile.TemporaryDirectory()
+print(temp_dir.name)
+print(type(temp_dir.name))
+# use temp_dir, and when done:
+# temp_dir.cleanup()
